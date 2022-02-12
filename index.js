c7fd4f869cafe57786d50a535bd66a4a2b1f5e82
yangzhiwen777 <65332557+yangzhiwen777@users.noreply.github.com>
2022-02-12 12:03:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const worker = new Worker('./worker.js');
 console.log('首页创建工作线程', worker);
+worker.postMessage({ action: 'computed' });
+worker.onmessage = function (e) {
+  console.log('主线程收到的消息', e);
+};
