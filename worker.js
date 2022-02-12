af44624af3854a027684c6d5bd96221dc402233a
yangzhiwen777 <65332557+yangzhiwen777@users.noreply.github.com>
2022-02-12 13:05:24 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,8 +2,8 @@
 console.log('我是工作线程,我要执行了');
 
 self.addEventListener('message', function (e) {
-  console.log('工作线程收到的消息', e)
+  console.log('工作线程收到的消息', e.data)
   console.log('我需要做一些事情')
   
-  self.postMessage('You said: ' + e.data);
+  self.postMessage(e.data);
 }, false);
