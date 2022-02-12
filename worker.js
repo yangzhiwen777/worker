7eaeebf69ef3f42048808cb70b5c460170a0a6d4
yangzhiwen777 <65332557+yangzhiwen777@users.noreply.github.com>
2022-02-12 17:18:29 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,30 @@
+self.onmessage = function (message) {
+  const { title, x0, y0, perPixel, maxIterations } = message.data;
+  const { width, height } = title;
+  const imageData = new ImageData(width, height);
+  const iterations = new Uint32Array(imageData.data.buffer);
+
+  let index = 0,
+    max = 0,
+    min = maxIterations;
+  for (let row = 0; (y = y0), row < height; row++, y += perPixel) {
+    for (let column = 0, x = x0; column < width; column++, x += perPixel) {
+      let n;
+      let r = x,
+        i = y;
+      for (n = 0; n < maxIterations; n++) {
+        let rr = r * r,
+          ii = i * i;
+        if (rr + ii > 4) {
+          break;
+        }
+        i = 2 * r * i + y;
+        r = rr - ii + x;
+      }
+      iterations[index++] = n;
+      if (n > max) max = n;
+      if (n < min) min = n;
+      postMessage({ title, imageData, min, max }, [imageData.data.buffer]);
+    }
+  }
+};
